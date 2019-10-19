5e0490650634975ad5df4eaa25ceb07563c8e949
LHongchao <jiye12yuran@126.com>
2019-10-19 10:50:19 +0800

    Initial commit

---
+++ task2_clustering/test.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from pandas import DataFrame, Series
+import numpy as np
+from numpy import *
+import matplotlib.pylab as plt
+
+class questions(object):
+    def __init__(self, questions_path):
+        self.question_dataset = questions_path
+
+    def load_dataset(self):
+        pass
+
+    def vocabulary_dataset(self):
+        pass
+
+    def keyword_list(self):
+        pass
+
+class question_clustering(object):
+    pass
+
+
+class question_prediction(object):
+    pass
+
+
+
+
